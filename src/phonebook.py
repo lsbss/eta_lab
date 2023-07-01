7fbf65cb091015b5e53adebe3781b2859dcdb6dd
Leonardo <leosbss@gmail.com>
2023-07-01 14:24:42 -0300

    atualizando implementacao e testes

--- src/phonebook.py
+++ src/phonebook.py
@@ -1,7 +1,11 @@
+from collections import OrderedDict
+
+
 class Phonebook:
 
     def __init__(self):
         self.entries = {'POLICIA': '190'}
+        self.invalid_char = ['#', '@', '!', '$', '%']
 
     def add(self, name, number):
         """
@@ -10,40 +14,34 @@ class Phonebook:
         :param number: number of person in string
         :return: 'Nome invalido' or 'Numero invalido' or 'Numero adicionado'
         """
-        if '#' in name:
-            return 'Nome invalido'
-        if '@' in name:
-            return 'Nme invalido'
-        if '!' in name:
-            return 'Nome invalido'
-        if '$' in name:
-            return 'Nome invalio'
-        if '%' in name:
-            return 'Nome invalido'
 
-        if len(number) < 0:
-            return 'Numero invalid'
+        for nome in self.invalid_char:
+            if nome in name:
+                return 'Nome invalido'
+
+        if not number:
+            return 'Numero invalido'
+
+        if not name:
+            return 'Nome invalido'
 
         if name not in self.entries:
             self.entries[name] = number
+            return 'Numero adicionado'
 
-        return 'Numero adicionado'
+        return 'Numero já cadastrado'
 
     def lookup(self, name):
         """
         :param name: name of person in string
         :return: return number of person with name
         """
-        if '#' in name:
-            return 'Nome invaldo'
-        if '@' in name:
-            return 'Nome invalido'
-        if '!' in name:
-            return 'Nme invalido'
-        if '$' in name:
-            return 'Nome invalido'
-        if '%' in name:
-            return 'Nome nvalido'
+        for nome in self.invalid_char:
+            if nome in name:
+                return 'Nome invalido'
+
+        if name not in self.entries:
+            return 'Nome nao encontrado'
 
         return self.entries[name]
 
@@ -52,14 +50,14 @@ class Phonebook:
 
         :return: return all names in phonebook
         """
-        return self.entries.keys()
+        return list(self.entries.keys())
 
     def get_numbers(self):
         """
 
         :return: return all numbers in phonebook
         """
-        return self.entries.values()
+        return list(self.entries.values())
 
     def clear(self):
         """
@@ -77,7 +75,7 @@ class Phonebook:
         """
         result = []
         for name, number in self.entries.items():
-            if search_name not in name:
+            if search_name in name:
                 result.append({name, number})
         return result
 
@@ -86,20 +84,46 @@ class Phonebook:
 
         :return: return phonebook in sorted order
         """
-        return self.entries
-
+        return sorted(self.entries.items())
     def get_phonebook_reverse(self):
         """
 
         :return: return phonebook in reverse sorted order
         """
-        return self.entries
-
+        return sorted(self.entries.items(), reverse=True)
     def delete(self, name):
         """
         Delete person with name
         :param name: String with name
         :return: return 'Numero deletado'
         """
+        if name not in self.entries:
+            return 'Numero nao encontrado'
+
         self.entries.pop(name)
         return 'Numero deletado'
+
+    def change_number(self, name, number):
+        """
+        Change number value by name
+        :param name: String with name
+        :param number: String with number
+        :return: return 'Numero editado'
+        """
+        if name in self.entries:
+            self.entries[name] = number
+            return 'Numero editado'
+
+        return 'Nome nao encontrado'
+
+    def get_name_by_number(self, number):
+        """
+        Return name value by number
+        :param name: String with name
+        :param number: String with number
+        :return: return Name
+        """
+        if number not in self.entries.values():
+            return 'Numero nao encontrado'
+
+        return list(self.entries.keys())
--- test/test_phonebook.py
+++ test/test_phonebook.py
@@ -1,4 +1,302 @@
-class TestPhonebook:
+from unittest import TestCase
+
+import pytest
+
+from src.phonebook import Phonebook
+
+
+class TestPhonebook(TestCase):
 
     def test_add(self):
-        assert False
+        name = 'BOMBEIRO'
+        number = '193'
+        result = 'Numero adicionado'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_existing(self):
+        name = 'POLICIA'
+        number = '190'
+        result = 'Numero já cadastrado'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_incorret_value_at_sign(self):
+        name = 'BOMBEIR@'
+        number = '192'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_incorret_value_hash(self):
+        name = 'BOMBEIR#'
+        number = '192'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_incorret_value_exclamation(self):
+        name = 'BOMBEIR!'
+        number = '192'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_incorret_value_dolar_sign(self):
+        name = 'BOMBEIR$'
+        number = '192'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_incorret_value_percentage(self):
+        name = 'BOMBEIR%'
+        number = '192'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_no_number(self):
+        name = 'POLICIA'
+        number = ''
+        result = 'Numero invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_add_no_name(self):
+        name = ''
+        number = '193'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.add(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_lookup(self):
+        name = 'POLICIA'
+        result = '190'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_not_found(self):
+        name = 'NOTFOUND'
+        result = 'Nome nao encontrado'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_hash(self):
+        name = 'BOMBEIR#'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_at_sign(self):
+        name = 'BOMBEIR@'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_exclamation(self):
+        name = 'BOMBEIR!'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_dolar_sign(self):
+        name = 'BOMBEIR$'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_lookup_percentage(self):
+        name = 'BOMBEIR%'
+        result = 'Nome invalido'
+
+        pb = Phonebook()
+
+        found_result = pb.lookup(name=name)
+        self.assertEqual(result, found_result)
+
+    def test_get_names(self):
+        name = 'BOMBEIRO'
+        number = '193'
+        result = ['POLICIA', 'BOMBEIRO']
+
+        pb = Phonebook()
+        pb.add(name=name, number=number)
+
+        found_result = pb.get_names()
+        self.assertEqual(result, found_result)
+
+    def test_get_numbers(self):
+        name = 'BOMBEIRO'
+        number = '193'
+        result = ['190', '193']
+
+        pb = Phonebook()
+        pb.add(name=name, number=number)
+
+        found_result = pb.get_numbers()
+        self.assertEqual(result, found_result)
+
+    def test_clear(self):
+        result = 'phonebook limpado'
+
+        pb = Phonebook()
+
+        found_result = pb.clear()
+        self.assertEqual(result, found_result)
+
+    def test_search(self):
+        name = 'POL'
+        result = [{'POLICIA', '190'}]
+
+        pb = Phonebook()
+
+        found_result = pb.search(name)
+        self.assertEqual(result, found_result)
+
+    def test_search_not_found(self):
+        name = 'XXX'
+        result = []
+
+        pb = Phonebook()
+
+        found_result = pb.search(name)
+        self.assertEqual(result, found_result)
+
+    def test_phonebook_sorted(self):
+        name = 'BOMBEIRO'
+        number = '193'
+        name_ = 'MARINHA'
+        number_ = '185'
+        result = [('BOMBEIRO', '193'), ('MARINHA', '185'), ('POLICIA', '190')]
+
+        pb = Phonebook()
+        pb.add(name=name, number=number)
+        pb.add(name=name_, number=number_)
+
+        found_result = pb.get_phonebook_sorted()
+        self.assertEqual(result, found_result)
+
+    def test_phonebook_reverse(self):
+        name = 'BOMBEIRO'
+        number = '193'
+        name_ = 'MARINHA'
+        number_ = '185'
+        result = [('POLICIA', '190'), ('MARINHA', '185'), ('BOMBEIRO', '193')]
+
+        pb = Phonebook()
+        pb.add(name=name, number=number)
+        pb.add(name=name_, number=number_)
+
+        found_result = pb.get_phonebook_reverse()
+        self.assertEqual(result, found_result)
+
+    def test_delete(self):
+        name = 'POLICIA'
+        result = 'Numero deletado'
+        pb = Phonebook()
+
+        found_result = pb.delete(name)
+        self.assertEqual(result, found_result)
+
+    def test_delete_not_found(self):
+        name = 'XXXX'
+        result = 'Numero nao encontrado'
+        pb = Phonebook()
+
+        found_result = pb.delete(name)
+        self.assertEqual(result, found_result)
+
+    def test_change_number(self):
+        name = 'POLICIA'
+        number = '191'
+        result = 'Numero editado'
+
+        pb = Phonebook()
+
+        found_result = pb.change_number(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_change_number_persist_value(self):
+        name = 'POLICIA'
+        number = '191'
+        result = ['191']
+
+        pb = Phonebook()
+
+        pb.change_number(name=name, number=number)
+        found_result = pb.get_numbers()
+        self.assertEqual(result, found_result)
+
+    def test_change_number_not_found(self):
+        name = 'XXX'
+        number = '191'
+        result = 'Nome nao encontrado'
+
+        pb = Phonebook()
+
+        found_result = pb.change_number(name=name, number=number)
+        self.assertEqual(result, found_result)
+
+    def test_get_name_by_number(self):
+        number = '190'
+        result = ['POLICIA']
+
+        pb = Phonebook()
+
+        found_result = pb.get_name_by_number(number=number)
+        self.assertEqual(result, found_result)
+
+    def test_get_name_by_number_not_found(self):
+        number = '999'
+        result = 'Numero nao encontrado'
+
+        pb = Phonebook()
+
+        found_result = pb.get_name_by_number(number=number)
+        self.assertEqual(result, found_result)
